40ca1f9ccbe9d7efb1e4290ff53e831909b01170
Sax Theowl <saxtheowl@gmail.com>
2021-04-07 22:43:46 +0200

    ok

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
+Play around with the bitcoin software, the idea being to make a tx from address A to B from scratch
 inspired from george hotz https://www.youtube.com/watch?v=1SWJaiY-8Y0
+
+https://github.com/Saxtheowl/hotz_bitcoin_ytb/blob/main/coin.png
--- tx.py
+++ tx.py
@@ -1,6 +1,8 @@
-import os, binascii, hashlib, base58, ecdsa, random, qrcode
-
-# QR CODE
+import os, binascii, hashlib, base58, ecdsa
+import random
+import socket
+import struct
+import time
 
 # import qrcode
 # img = qrcode.make(publ_addr)
@@ -9,9 +11,11 @@ import os, binascii, hashlib, base58, ecdsa, random, qrcode
 def shex(x):
     return binascii.hexlify(x).decode()
 
+def checksum(x):
+    return hashlib.sha256(hashlib.sha256(x).digest()).digest()[:4]
+
 def b58checksum(x):
-    checksum = hashlib.sha256(hashlib.sha256(x).digest()).digest()[:4]
-    return base58.b58encode(x + checksum)
+    return base58.b58encode(x + checksum(x))
 
 def ripemd160(x):
     d = hashlib.new('ripemd160')
@@ -39,8 +43,40 @@ def get_key_with_seed(seed = 1337):
 priv_key, WIF, publ_addr = get_key_with_seed(1337)
 _, _, publ_addr2 = get_key_with_seed(1338)
 _, _, publ_addr3 = get_key_with_seed(6020576873535702996985333164707475418485818481759134379752425601533599923418591834955793927209263343179263601441637535091654337771743703275503669893180678037828190108838657560209946521262509792282192887039426181736226204416607422926108649983)
+
+magic = 0xe8f3e1e3
+
+def makeMessage(magic, command, payload):
+#    checksum = hashlib.sha256(hashlib.sha256(payload).digest()).digest()[0:4]
+    return struct.pack('L12sL4s', magic, command, len(payload), checksum(payload)) + payload
+
+def getVersionMsg():
+    version = 60002
+    services = 1
+    timestamp = int(time.time())
+    addr_me = b"\x00" * 26
+    addr_you = b"\x00" * 26
+    nonce = random.getrandbits(64)
+    sub_version_num = '\x00'
+
+    start_height = 0
+
+    payload = struct.pack('<LQQ26s26sQsL', version, services, timestamp, addr_me,
+        addr_you, nonce, sub_version_num, start_height)
+    return makeMessage(magic, 'version', payload)
+
+if __name__ == "__main__":
+    peers = socket.gethostbyname_ex('seed.bitcoinabc.org')[2]
+    peer = random.choice(peers) # wtf does the return take x time to be random
+    print(peer)
+    
+    exit(0)
+    
+    print("%s -> %s" % (publ_addr, publ_addr2))
+    print(publ_addr3)
+
 #print("privateKey is: {}".format(shex(priv_key)))
 #print("WIF is: {}".format(WIF))
 #print("pubkey is: {}".format(publ_key))
 #print("publ_addr is: {}".format(publ_addr, publ_addr2))
-print("%s -> %s" % (publ_addr, publ_addr2))
+#QR code
