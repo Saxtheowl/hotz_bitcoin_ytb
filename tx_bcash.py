b453d154859df0ea0d01b3b74b7fca38761d873c
Sax Theowl <saxtheowl@gmail.com>
2021-04-11 10:20:02 +0200

    ok

--- tx_bcash.py
+++ tx_bcash.py
@@ -12,7 +12,7 @@ from hexdump import hexdump
 BITCOIN_MAGIC = 0xd9b4bef9
 BITCOIN_SEED = 'dnsseed.bluematt.me'
 BCASH_MAGIC = 0xe8f3e1e3
-BCASH_SEED = 'seed.bitcoinabc.org'
+BCASH_SEED = 'seeder.fabien.cash'
 
 # tx id:
 # b24c3c1be47420e9d55f6fd17fe38757383eb47d0623cf276008ab5ce8887916
@@ -25,7 +25,8 @@ BCASH_SEED = 'seed.bitcoinabc.org'
 # qq7rl4g7j738pxdmrqn5j8z69l2gxycy0vgvay6uxd
 
 def shex(x):
-    return binascii.hexlify(x).decode()
+#    print("hexlify = %s decode = %s" % ( binascii.hexlify(x),  binascii.hexlify(x).decode()))
+    return binascii.hexlify(x)
 
 def checksum(x):
     return hashlib.sha256(hashlib.sha256(x).digest()).digest()[:4]
@@ -57,21 +58,21 @@ def get_key_with_seed(seed = 1337):
     publ_key = b'\x04' + vk.to_string()
     hash160 = ripemd160(hashlib.sha256(publ_key).digest()).digest()
     publ_addr = b58checksum( b'\x00' + hash160)
-    return priv_key, WIF, publ_addr
+    return priv_key, WIF, hash160, publ_addr
 
-priv_key, WIF, publ_addr = get_key_with_seed()
-_, _, publ_addr2 = get_key_with_seed(1338)
+priv_key, WIF, hash1601, publ_addr = get_key_with_seed()
+_, _, _, publ_addr2 = get_key_with_seed(1338)
 
 dat_secret_seed = read_integer('secret')
 
-_, WIF3, publ_addr3 = get_key_with_seed(dat_secret_seed)
-_, WIF4, publ_addr4 = get_key_with_seed(dat_secret_seed + 1337)
+_, WIF3, hash1603, publ_addr3 = get_key_with_seed(dat_secret_seed)
+_, WIF4, hash1604, publ_addr4 = get_key_with_seed(dat_secret_seed + 1337)
 
 def makeMessage(command, payload):
     return struct.pack('<L12sL4s', BCASH_MAGIC, command, len(payload), checksum(payload)) + payload
 
 def getVersionMsg():
-    version = 170002
+    version = 180002
     services = 1
     timestamp = int(time.time())
     addr_me = b"\x00" * 26
@@ -87,7 +88,7 @@ def getVersionMsg():
 def getTxMsg(tx_in, tx_out):
     version = 1
     locktime = 0
-    payload = struct.pack('<LBsBsL', version, 1, tx_in, 1, tx_out, locktime)
+    payload = struct.pack('<LB', version, 1) + tx_in + b'\x01' + tx_out + struct.pack('<L', locktime)
     return makeMessage(b'tx', payload)
 
 def sock_read(sock, count):
@@ -101,8 +102,11 @@ def recvMessage(sock):
     assert magic == BCASH_MAGIC
     payload = sock_read(sock, plen)
     assert checksum(payload) == cksum
-    print(command)
-    hexdump(payload)
+    if len(payload) > 0x10:
+        print("%s %d" % (command, len(payload)))
+    else:
+        print(command)
+        hexdump(payload)
     return command, payload
 
 if __name__ == "__main__":
@@ -110,38 +114,33 @@ if __name__ == "__main__":
     peers = socket.gethostbyname_ex(BCASH_SEED)[2]
     peer = random.choice(peers) # wtf does the return take x time to be random
     print("peer is:{}".format(peer))
-    print("we send from %s to %s" % (publ_addr3, publ_addr4))
-    
-    vermsg = getVersionMsg()
-    hexdump(vermsg)
-
+    print("hash160 shex is %s" % (shex(hash1601)))
+    print("we send from %s to %s" % (publ_addr, publ_addr2))
+#    exit(0)
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((peer, 8333))
-    sock.send(vermsg)
+    sock.send(getVersionMsg())
     cmd, payload = recvMessage(sock)
     cmd, payload = recvMessage(sock)
-
-#    genesis_block = binascii.unhexlify('000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f')[::-1]
-    my_block = binascii.unhexlify('000000000000000002ca6e4f04ceeb582b237c118e1df18695ee344bc7919cd2')[::-1]
-    
+    print("send verack")
     msg = makeMessage(b'verack', b'')
     sock.send(msg)
-    print("send verack")
     cmd, payload = recvMessage(sock)
     cmd, payload = recvMessage(sock)
     cmd, payload = recvMessage(sock)
     cmd, payload = recvMessage(sock)
     cmd, payload = recvMessage(sock)
-    
-    msg = makeMessage(b'getdata', struct.pack('<BL32s', 1, 2, my_block[::-1]))
-    hexdump(msg)
-    sock.send(msg)
-    cmd, payload = recvMessage(sock)
-    txes = payload[4+32+32+4+4+4:]
-    hexdump(txes[0:0x100])
 
-    idx = txes.find(binascii.unhexlify('b24c3c1be47420e9d55f6fd17fe38757383eb47d0623cf276008ab5ce8887916'))
+#    genesis_block = binascii.unhexlify('000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f')
+    my_block = binascii.unhexlify('000000000000000002ca6e4f04ceeb582b237c118e1df18695ee344bc7919cd2')
+    his_block = binascii.unhexlify('000000000000000001b9d2f1286800f49908901f6d2259d5c09f0ba7716a53b6')
+    msg = makeMessage(b'getdata', struct.pack('<BL32s', 1, 2, his_block[::-1]))
+    sock.send(msg)
+    
+    cmd, payload = recvMessage(sock)
+    idx = payload.find(hash1601)
     print(idx)
+    hexdump(payload[idx-100:idx+0x100])
     
 #    exit(0)
    
